50962341e8e31521d440aa1e10c39104a40e59a2
JosiasSd9-sys <josias.sd9@gmail.com>
2026-06-25 15:17:51 -0300

    fix: remove import.meta.url incompatível com esbuild cjs

--- server.ts
+++ server.ts
@@ -1,12 +1,8 @@
 import express from "express";
 import path from "path";
 import fs from "fs/promises";
-import { fileURLToPath } from "url";
 import { createServer as createViteServer } from "vite";
 import { geminiService } from "./src/services/geminiService.impl.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
